667b6fd74f0bbc5bc01ca51e94e6cd7d8ecae1bc
Harshvardhan Rao <51167811+plantedbrain@users.noreply.github.com>
2020-10-24 17:08:34 +0530

    Update word2vev.py

--- word2vev.py
+++ word2vev.py
@@ -12,29 +12,15 @@ from nltk.corpus import stopwords
 
 import re
 
-paragraph = """I have three visions for India. In 3000 years of our history, people from all over 
-               the world have come and invaded us, captured our lands, conquered our minds. 
-               From Alexander onwards, the Greeks, the Turks, the Moguls, the Portuguese, the British,
-               the French, the Dutch, all of them came and looted us, took over what was ours. 
-               Yet we have not done this to any other nation. We have not conquered anyone. 
-               We have not grabbed their land, their culture, 
-               their history and tried to enforce our way of life on them. 
-               Why? Because we respect the freedom of others.That is why my 
-               first vision is that of freedom. I believe that India got its first vision of 
-               this in 1857, when we started the War of Independence. It is this freedom that
-               we must protect and nurture and build on. If we are not free, no one will respect us.
-               My second vision for India’s development. For fifty years we have been a developing nation.
-               It is time we see ourselves as a developed nation. We are among the top 5 nations of the world
-               in terms of GDP. We have a 10 percent growth rate in most areas. Our poverty levels are falling.
-               Our achievements are being globally recognised today. Yet we lack the self-confidence to
-               see ourselves as a developed nation, self-reliant and self-assured. Isn’t this incorrect?
-               I have a third vision. India must stand up to the world. Because I believe that unless India 
-               stands up to the world, no one will respect us. Only strength respects strength. We must be 
-               strong not only as a military power but also as an economic power. Both must go hand-in-hand. 
-               My good fortune was to have worked with three great minds. Dr. Vikram Sarabhai of the Dept. of 
-               space, Professor Satish Dhawan, who succeeded him and Dr. Brahm Prakash, father of nuclear material.
-               I was lucky to have worked with all three of them closely and consider this the great opportunity of my life. 
-               I see four milestones in my career"""
+paragraph ="""In the eighties, children always used to ask me questions such as “When can I sing the Song of India?” Today, the youth are asking me, “What I can give to India?” This shows that the nation is on a positive growth trajectory.
+The change in pattern of the questions is indicative of the transformation which has taken place over the years. Also, I receive many e-mails and letters asking me “what I can give to India, my country?”
+When I study the letters, messages and mails that I have received and also the personal interactions with the people, I can clearly see abundant opportunities in which every citizen can contribute. I thought of sharing this with you: My topic of this address will be — “What can I give to my nation?"
+In Indian history, our nation has come across a situation, all at a time, an ascending economic trajectory, continuously rising foreign exchange reserves, increasing domestic investment with investors’ confidence rising steadily, global successes of Indian managerial and entrepreneurial talents, global recognition of technological competence, energy of 540 million youth, umbilical connectivities of more than 25 million people of Indian origin in various parts of the planet and the interest shown by many developed countries to invest in our engineers and scientists through setting up of new Research and Development Centres in India.
+The distinction between the public and the private sectors and the illusory primacy of one over the other is vanishing. Also, there is a trend that many young people are opting for creating new enterprises instead of being mere employees.
+Providing leadership for the one billion people with multi-cultural, multi-language and multi-religious backgrounds is indeed the core competence of our nation. Our technological competence and value systems with civilisational heritage are highly respected by the world community. Also, Foreign Institutional Investors find investing in India attractive as the returns are high and assured. Indian industrialists are also investing abroad and opening new business ventures.
+Our Gross Domestic Product which stands at US $729 billion is poised to grow at 10% annually which along with various other concurrent actions, will enhance the welfare of farmers, workers, professionals and unleash creativity of entrepreneurs, business persons, scientists, engineers and all other constituents  of society.
+Today due to the open sky policy and competitiveness air travel has become affordable for the growing middle class. The Railways have introduced many improvements and people can book tickets through the Internet. The revolution in travel has not only connected people but also boosted tourism and the economy. Tele-density in the country has gone up to 18%."""
+               
 
 
 
@@ -61,7 +47,7 @@ model = Word2Vec(sentences, min_count=1)
 words = model.wv.vocab
 
 # Finding Word Vectors
-vector = model.wv['war']
+vector = model.wv['policy']
 
 # Most similar words
-similar = model.wv.most_similar('vikram')
+similar = model.wv.most_similar('india')
